e44b4a4925440072bf708ace21500cb64976ea42
IsardYsanne <78618492+IsardYsanne@users.noreply.github.com>
2021-09-09 20:22:45 +0500

    Add footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+        <div id="footer">
+            <h2>Подвал сайта</h2>
+        </div>
+    </div>
+    <!-- Проверка на работоспособность -->
+    <!-- <script type="text/javascript">
+        if (window.jQuery) alert("jQuery подключен");
+        else alert("jQuery не подключен");
+    </script>  -->
+</body>
+</html>

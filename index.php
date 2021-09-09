f621f22c53dddc6cb476f057e0d82784177a80ca
IsardYsanne <78618492+IsardYsanne@users.noreply.github.com>
2021-09-09 20:19:37 +0500

    add index.php
    It concatenates header, content, footer

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    require_once("header.php");
+?>
+        <div id="content" class="content-block">
+            <h2>Демонстрация работы формы</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore obcaecati ipsam, sapiente vel ut unde quisquam, placeat temporibus in, optio mollitia. Dolore mollitia nam, neque ea facilis nihil dolores molestias.</p>
+        </div>
+<?php
+    require_once("footer.php");
+?>

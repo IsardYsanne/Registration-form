d5e0bb63a9474faccc0e81ca2c023cd0b81efe20
IsardYsanne <78618492+IsardYsanne@users.noreply.github.com>
2021-09-09 20:47:08 +0500

    Add logout.php
    The button "Выйти" handler

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+
+    session_start();
+
+    unset($_SESSION["email"]);
+    unset($_SESSION["password"]);
+
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: ".$_SERVER["HTTP_REFERER"]); // Возвращаем пользователя на ту страницу, на которой он нажал на кнопку выход.
+?>
